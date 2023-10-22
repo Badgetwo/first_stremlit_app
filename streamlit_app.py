9e9476b0279e1dadc9fc37eeb0b6616a63aaed15
Badgetwo <148726625+Badgetwo@users.noreply.github.com>
2023-10-23 00:18:09 +0100

    Update streamlit_app.py
    Corrected subtitle to header
    Added breakfast items in text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.subtitle('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+stremlit.text('Omega 3 & Blueberry Oatmeal')
+stremlit.text('Hard-boiled Free Range Egg')
+stremlit.text('Kale, spinach & Rocket Smoothie')
