f65376e2cbf1bbbe30e788b9e65d7a09f7b33c31
Badgetwo <148726625+Badgetwo@users.noreply.github.com>
2023-10-23 00:21:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Hard-boiled Free Range Egg')
-streamlit.text('Kale, spinach & Rocket Smoothie')
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔Hard-boiled Free Range Egg')
+streamlit.text('🥑🍞Kale, spinach & Rocket Smoothie')
+  
